1aa759e3f00048c1eaebfbc684d4b294092a862d
abdumalikovv <abduvohidabdumalikov6@gmail.com>
2021-11-03 17:46:53 +0600

    frist commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var userName = prompt("Enter your name");
+var userSurname = prompt("Enter your sure name");
+var userFatherName = prompt("Enter your father's name");
+var userNationality = prompt("Enter your nationality");
+var userAge = prompt("Enter your age");
+var userPlaceOfBirth = prompt("Enter your place of birth");
+var userPhoneNumber = prompt("Enter the phone number");
+var userDoYouWorkOrStudy = prompt("Do you work or study? ");
+var userWhatDoYouDoInYourFreeTime = prompt("What do you do in your free time?");
+var userWhatDoYourFavoriteFood = prompt("what dio your favoruite food");
+
+var fullinfo = 
+"Name" + " " + yourName + "\n" +
+"SurName" + " " + yourSurName + "\n" +
+"FattherName" + " " + yourFatherName + "\n" +
+"Nationality" + " " + yourNationality + "\n" +
+"PlaceOfBirth" + " " + yourPlaceOfBirth + "\n" +
+"PhoneNumber" + " " + yourPhoneNumber + "\n" +
+"DoYouWorkOrStudy" + " " + yourDoYouWorkOrStudy + "\n" +
+"WhatDoYouDoInYourFreeTime" + " " + yourWhatDoYouDoInYourFreeTime+ "\n" +
+"WhatDoYourFavoriteFood" + " " + yourWhatDoYourFavoriteFood+ "\n" ;
+
+
+console.log(fullinfo);
+
+alert(fullinfo);
+
+
